cfdeb213c1c6fc4de968e92ed3d7a13649acefbc
Tan Jin Daat <92740025+Jin-Daat@users.noreply.github.com>
2022-04-18 21:48:37 +0800

    Completed question 2

--- Question2.java
+++ Question2.java
@@ -8,9 +8,22 @@ public class Question2
      * Prompts user for height and weight, calculate and print out the BMI of the user
      * 
      * Hint: in.nextDouble() to read double
-     */
-     
-    Scanner in = new Scanner(System.in);
-    double height = in.nextDouble();
+     */    
+    //Question 2
+    //Create scanner Object
+    Scanner sc = new Scanner(System.in);
+
+    //Prompt user for height
+    System.out.print("Enter your height in meters: ");
+    //Get user height 
+    double height = sc.nextDouble();
+    //Prompt user for weight
+    System.out.print("Enter your weight in kilograms: ");
+    //Get user weight
+    double weight = sc.nextDouble();
+
+    //Calculate and print the bmi
+    System.out.println("Your BMI is: " + (weight/(height*height)));
+
   }
 }
