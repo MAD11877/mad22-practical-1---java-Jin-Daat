c48da8fac6facf160d4967d8f7b38f10b73a80c3
Tan Jin Daat <92740025+Jin-Daat@users.noreply.github.com>
2022-04-18 22:37:19 +0800

    Completed Question 5

--- Question5.java
+++ Question5.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.*
 
 public class Question5
 {
@@ -27,6 +28,47 @@ public class Question5
      */
      
     Scanner in = new Scanner(System.in);
+    //Create an empty list to store the numbers
+    List<Integer> numList = new ArrayList<Integer>();
+    //Maximum count
+    int maxCount = 0;
+    //Modal number
+    int mode = 0;
+    
+    //Get user input for the number of prompts
+    System.out.print("Number of prompts: ");
+    int promptNum = in.nextInt();
+    
+    //Start the loop to key in number depending on number of prompt
+    for (int i = 0; i < promptNum; i ++){
+        //Ask user for a number
+        System.out.print("\nEnter a number: ");
+        int num = in.nextInt();
+        //Add the number to the list
+        numList.add(num);
+    }
+    
+    //Start to check and fine modal number
+    //Use an outer loop to choose the number to iterate through
+    for (int i = 0; i < numList.size(); i ++){
+        //Set constant count
+        //Reset Count
+        int count = 0;
+        //Inner loop to run through all varaibles in the list 
+        for (int j = 0; j < numList.size(); j++){
+            //Condition if values are equal
+            if (numList.get(i) == numList.get(j)){
+                count ++;
+            }
+        }
+        //Condition to check maxCount and modal number. Reassign if neccessary.
+        if (count > maxCount){
+            maxCount = count;
+            mode = numList.get(i);
+        }
+    }
+    //Print the final result 
+    System.out.println("Modal number is : " + mode);
     
   }
 }
